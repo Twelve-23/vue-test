adbd302e8f6be71b1a12c26fb85279ec8cb3e3d6
vishwasPHPDots <vishwas@phpdots.com>
2021-10-19 18:14:06 +0530

    optimized the code and changed the indetantion

--- src/store/index.js
+++ src/store/index.js
@@ -6,34 +6,34 @@ import data from './../../src/assets/data'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-	state: {
-		data: data
-	},
-	getters: {
-		moreOrders: (state) => {
-			if (state.data && state.data.periods) {
-				const moreOrders = []
-				for (const period of state.data.periods) {
-					if (period.itemized) {
-						for (const orderId in period.itemized) {
-							if (Object.hasOwnProperty.call(period.itemized, orderId)) {
-								const orderSummary = period.itemized[orderId];
-								console.log("😎 ~ file: index.js ~ line 21 ~ orderSummary", (orderSummary.orders > 20) ? 'true' : 'false')
-								if (orderSummary.orders > 20) {
-									console.log('here')
-									moreOrders.push(orderId)
-								}
-							}
-						}
-					}
-				}
+  state: {
+    data: data
+  },
+  getters: {
+    moreOrders: (state) => {
+      if (state.data && state.data.periods) {
+        const moreOrders = []
+        const lastPeriodIndex = state.data.periods.length - 1
+        for (let key = 0; key < lastPeriodIndex; key++) {
+          const period = state.data.periods[key]
+          if (period.itemized) {
+            for (const orderId in period.itemized) {
+              if (Object.hasOwnProperty.call(period.itemized, orderId)) {
+                const orderSummary = period.itemized[orderId]
+                if (orderSummary.orders > 20) {
+                  moreOrders.push(orderId)
+                }
+              }
+            }
+          }
+        }
 
-				return moreOrders
-			}
+        return moreOrders
+      }
 
-			return []
-		}
-	}
+      return []
+    }
+  }
 })
 
 export default store
